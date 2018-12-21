218e67b7b18f50a115d2d5fb4c816d9894da22df
Paulo Mello <paulo.mello.simepar@gmail.com>
2018-12-21 10:42:51 -0200

    Ok

--- gfsDownloader.py
+++ gfsDownloader.py
@@ -1,85 +1,90 @@
 from datetime import datetime
 
+
 class GFS(object):
-	from urllib.request import urlretrieve
-	from progress.bar import Bar
-	from progress.spinner import Spinner
-	
-	"""docstring for GFS"""
-	def __init__(self, startDate, forecast, resolution):
-		self.startDate = startDate
-		utcTime = startDate.strftime('%H')
-		self.forecast = int(forecast)
-		self.resolution = resolution
+    from urllib.request import urlretrieve
+    from progress.bar import Bar
+    from progress.spinner import Spinner
 
-	def __get(self, url, to):
-		self.p = None
+    """docstring for GFS"""
 
-		def __update(blocks, bs, size):
-			if not self.p:
-				if size < 0:
+    def __init__(self, startDate, forecast, resolution):
+        self.startDate = startDate
+        utcTime = startDate.strftime('%H')
+        self.forecast = int(forecast)
+        self.resolution = resolution
 
-					self.p = GFS.Spinner(to)
-				else:
+    def __get(self, url, to):
+        self.p = None
 
-					TOTALSIZE = int(size/1000000)
-					self.p = GFS.Bar(to, max=size, fill='=', suffix='{} MB | %(percent).1f%% - %(eta)ds'.format(TOTALSIZE))
-			else:
-				if size < 0:
-					self.p.update()
-				else:
-					self.p.goto(blocks * bs)
+        def __update(blocks, bs, size):
+            if not self.p:
+                if size < 0:
 
-		GFS.urlretrieve(url, to, __update)
-		self.p.finish()
+                    self.p = GFS.Spinner(to)
+                else:
 
-	def Download(self):
-		link = "http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/"
-		fileList = ["gfs.{}{}/gfs.t{}z.pgrb2.0p{}.f{}".format(self.startDate.strftime('%Y%m%d'), 
-			                                                                "00", "00", 
-			                                                                self.resolution, str(forecasting).zfill(3) )  \
-		for forecasting in range(0, self.forecast+1, 3)]
+                    TOTALSIZE = int(size/1000000)
+                    self.p = GFS.Bar(
+                        to, max=size, fill='=', suffix='{} MB | %(percent).1f%% - %(eta)ds'.format(TOTALSIZE))
+            else:
+                if size < 0:
+                    self.p.update()
+                else:
+                    self.p.goto(blocks * bs)
 
-		for fileLink in fileList:
-			
-			self.__get(link+fileLink, fileLink.split("/")[1])
+        GFS.urlretrieve(url, to, __update)
+        self.p.finish()
+
+    def Download(self):
+        link = "http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod/"
+        fileList = ["gfs.{}{}/gfs.t{}z.pgrb2.0p{}.f{}".format(self.startDate.strftime('%Y%m%d'),
+                                                              "00", "00",
+                                                              self.resolution, str(forecasting).zfill(3))
+                    for forecasting in range(0, self.forecast+1, 3)]
+
+        for fileLink in fileList:
+
+            self.__get(link+fileLink, fileLink.split("/")[1])
+
 
 def cliArguments(cmd=None):
-	import argparse as args
-	from sys import argv
-	from datetime import datetime
+    import argparse as args
+    from sys import argv
+    from datetime import datetime
 
-	parser = args.ArgumentParser("Loading GFS Downloader arguments...")
-	parser.add_argument("-d", "--date",
-			                required=True,
-			                dest="date",
-			                type=lambda d: datetime.strptime(d, '%Y%m%d_%H'),
-			                help="Datetime argument must be set example: -d 10-12-2018_10")
-	parser.add_argument("-f", "--forecast",
-			                required=True,
-			                dest="forecastRange",
-			                type=str,
-			                help="Forecsat range time,  must be set, example: -f 120")
-	parser.add_argument("-r", "--resolution",
-			                required=True,
-			                dest="resolutionModel",
-			                type=str,
-			                help="Spatial Resolution model, must be set, example: -f 25")
-	parser.add_argument("-o", "--outputFile",
-			                required=False,
-			                type=str,
-			                help="path to output the downloaded model files. example: -o /home/user/")
+    parser = args.ArgumentParser("Loading GFS Downloader arguments...")
+    parser.add_argument("-d", "--date",
+                        required=True,
+                        dest="date",
+                        type=lambda d: datetime.strptime(d, '%Y%m%d_%H'),
+                        help="Datetime argument must be set example: -d 10-12-2018_10")
+    parser.add_argument("-f", "--forecast",
+                        required=True,
+                        dest="forecastRange",
+                        type=str,
+                        help="Forecsat range time,  must be set, example: -f 120")
+    parser.add_argument("-r", "--resolution",
+                        required=True,
+                        dest="resolutionModel",
+                        type=str,
+                        help="Spatial Resolution model, must be set, example: -f 25")
+    parser.add_argument("-o", "--outputFile",
+                        required=False,
+                        type=str,
+                        help="path to output the downloaded model files. example: -o /home/user/")
 
-	return parser.parse_args(args=cmd)
+    return parser.parse_args(args=cmd)
+
 
 def main(cmd=None):
-	argsProcess = cliArguments(cmd=cmd)
-	return argsProcess.__dict__
+    argsProcess = cliArguments(cmd=cmd)
+    return argsProcess.__dict__
+
 
 if __name__ == '__main__':
-	dictArgs = main()
-	print(dictArgs['date'], dictArgs['forecastRange'])
-	teste = GFS(dictArgs['date'], dictArgs['forecastRange'], 
-		        resolution=dictArgs['resolutionModel'])
-	teste.Download()
-
+    dictArgs = main()
+    print(dictArgs['date'], dictArgs['forecastRange'])
+    teste = GFS(dictArgs['date'], dictArgs['forecastRange'],
+                resolution=dictArgs['resolutionModel'])
+    teste.Download()
